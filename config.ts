8bb16ade7c6b492491173d4dc029f181f8917fc8
Jonathan H Russell <jonathan.russell@htosolutions.com>
2025-02-28 14:14:32 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,1 @@
-export const dbName = 'bespry-dev'
-export const username = 'admin'
-export const password = '9ODRLGQLvta0MhZp9o7K'
-export const host = 'bespry-dev.cxco2e6i4vo9.us-east-2.rds.amazonaws.com'
 
